e990e29d4b95869da6980ab789850d5030ad3ef4
nehemias_PiedrahitaDeveloper <nehemiaspiedrahita1@gmail.com>
2026-04-23 10:55:55 -0500

    Update helloWorld.py

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,2 +1,22 @@
 print ("hello world!")
+a = 3
+c=10000
+print (a-c)
+nemo = "nehemias Piedrahita"
+print(nemo)
+b = 'h'
+print (f"{nemo} {b} {c}")
 
+for i in range(8):
+    print(i)
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+
+n1=4.3
+n2=5.0
+n3=2.9
+promedio = (n1+n2+n3)/3
+print (f"El promedio es: {promedio:.2f}")   
